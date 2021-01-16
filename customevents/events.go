ff658b23a4070c3c1b8bfc624aedbc3fc6985c84
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-01-16 10:58:03 +0545

    feat: update event param type

--- customevents/events.go
+++ customevents/events.go
@@ -3,11 +3,11 @@ package customevents
 import "log"
 
 // SendEmail sends the email
-func SendEmail(data interface{}) {
+func SendEmail(data string) {
 	log.Println("📨 Sending email with data: ", data)
 }
 
 // PayBills pays the bills
-func PayBills(data interface{}) {
+func PayBills(data string) {
 	log.Println("💲 Pay me a bill: ", data)
 }
