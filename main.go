6ce4db5321dbb9ca4d0aad99851197d665f14645
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-01-16 10:34:57 +0545

    refactor: clean up un-necessary code

--- main.go
+++ main.go
@@ -7,41 +7,35 @@ import (
 	"os/signal"
 
 	"github.com/joho/godotenv"
+
+	"github.com/dipeshdulal/event-scheduling/customevents"
 )
 
-// Test structure
-type Test struct {
-	ID   uint
-	Name string
+// Event structure
+type Event struct {
+	ID      uint
+	Name    string
+	Payload string
+	Locked  string
+}
+
+var eventListeners = map[string]func(d interface{}){
+	"SendEmail": customevents.SendEmail,
+	"PayBills":  customevents.PayBills,
 }
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+
 	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file ", err)
 	}
 
 	db := initDBConnection()
-
 	seedDB(db)
-
-	rows, err := db.Query("SELECT id, name FROM public.test")
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	go func() {
-		for rows.Next() {
-			var test Test
-			rows.Scan(&test.ID, &test.Name)
-			log.Printf("test: %v\n", test)
-		}
-	}()
-
-	signal.Notify(interrupt, os.Interrupt)
 
 	go func() {
 		for range interrupt {
