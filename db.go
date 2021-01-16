8d951b3c8d1a818f680d41eab94e88af5987f164
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-01-16 10:34:30 +0545

    refactor: added seed db to db.go file

--- db.go
+++ db.go
@@ -18,3 +18,14 @@ func initDBConnection() *sql.DB {
 
 	return db
 }
+
+func seedDB(db *sql.DB) error {
+	log.Print("💾 Seeding database with table...")
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS "public"."test" (
+			"id" integer NOT NULL,
+			"name" text NOT NULL
+		)
+	`)
+	return err
+}
